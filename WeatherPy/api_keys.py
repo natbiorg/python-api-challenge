977131b6ac5a47bda9ec6d0d48e44d39b2b0c311
Natalie <natalierg1199@gmail.com>
2024-07-21 18:56:41 -0500

    saved api keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "9a1b9f4208d6947bb27d738463de76b3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2a7c785332264ef08f4d849002f3f2d8"
